791d23f3b4c09dfed2cf3dfe01f0fcb8e5433378
Teona Maghradze <maghradzeteo@gmail.com>
2024-01-29 16:56:22 +0400

    add product table

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import TableList from "../../components/table/TableList";
 import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
@@ -20,6 +21,11 @@ function Home() {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+
+        <div className="list-container">
+          <div className="list-title">Latest Transactions</div>
+          <TableList />
         </div>
       </div>
     </div>
