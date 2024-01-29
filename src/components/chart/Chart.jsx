cbe56681626c697a80fc0c89ef0374b981f07876
Teona Maghradze <maghradzeteo@gmail.com>
2024-01-29 14:40:03 +0400

    add charts for months

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,7 +1,56 @@
 import "./Chart.scss";
+import {
+  LineChart,
+  AreaChart,
+  Area,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "January", Total: 1000 },
+  { name: "February", Total: 2200 },
+  { name: "March", Total: 500 },
+  { name: "April", Total: 1400 },
+  { name: "May", Total: 600 },
+  { name: "June", Total: 1500 },
+];
 
 function Chart() {
-  return <div className="chart">Chart</div>;
+  return (
+    <div className="chart">
+      <div className="title">Last 6 Months (number)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
 export default Chart;
