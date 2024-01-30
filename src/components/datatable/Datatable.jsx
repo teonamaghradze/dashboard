e02a6465566588c8b6d97d93a3d7739b2053b865
Teona Maghradze <maghradzeteo@gmail.com>
2024-01-30 12:15:43 +0400

    add table on users page

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,40 @@
+import "./Datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+
+import { userColumns, userRows } from "../../dataSource";
+
+function Datatable() {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cell-action">
+            <div className="view-button">View</div>
+            <div className="delete-button">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="datatable">
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 8 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+}
+
+export default Datatable;
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,19 @@
 import "./List.scss";
 
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Datatable from "../../components/datatable/Datatable";
+
 function List() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="list-container">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
 }
 
 export default List;
