8b7f0badcf7320404a100fc48ecffd16cc8fc19e
Teona Maghradze <maghradzeteo@gmail.com>
2024-01-30 13:46:25 +0400

    add upload image functionality

---
+++ src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "janedoe",
+  },
+  {
+    id: 2,
+    label: "Name and surname",
+    type: "text",
+    placeholder: "Jane Doe",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "mail",
+    placeholder: "jane_doe@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+8859999999",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "Station square #2. 0100 TBILISI",
+  },
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "Georgia",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,8 +2,13 @@ import "./New.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+import { useState } from "react";
 
-function New() {
+function New({ inputs, title }) {
+  const [file, setFile] = useState("");
+
+  console.log(file);
+
   return (
     <div className="new">
       <Sidebar />
@@ -12,12 +17,16 @@ function New() {
         <Navbar />
 
         <div className="top">
-          <h5>Add New User</h5>
+          <h5>{title}</h5>
         </div>
         <div className="bottom">
           <div className="left">
             <img
-              src="https://media.istockphoto.com/id/1401105799/vector/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-moment-placeholder.jpg?s=612x612&w=0&k=20&c=TW1ZU3X512_3lGWDFhEygj66CT1Ikj1-R51FCdwJ31E="
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://media.istockphoto.com/id/1401105799/vector/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-moment-placeholder.jpg?s=612x612&w=0&k=20&c=TW1ZU3X512_3lGWDFhEygj66CT1Ikj1-R51FCdwJ31E="
+              }
               alt="avatar"
             />
           </div>
@@ -28,46 +37,20 @@ function New() {
                 <label htmlFor="file">
                   Upload Image: <DriveFolderUploadIcon className="icon" />
                 </label>
-                <input type="file" id="file" style={{ display: "none" }} />
-              </div>
-
-              <div className="input-container">
-                <label>Username</label>
-                <input type="text" placeholder="janedoe" />
-              </div>
-
-              <div className="input-container">
-                <label>Name and surname</label>
-                <input type="text" placeholder="Jane Doe" />
-              </div>
-
-              <div className="input-container">
-                <label>Email</label>
-                <input type="email" placeholder="janedoe@gmail.com" />
-              </div>
-
-              <div className="input-container">
-                <label>Phone number</label>
-                <input type="text" placeholder="+99599999999" />
-              </div>
-
-              <div className="input-container">
-                <label>Password</label>
-                <input type="password" />
-              </div>
-
-              <div className="input-container">
-                <label>Address</label>
                 <input
-                  type="password"
-                  placeholder="Station square #2. 0100 TBILISI"
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                  onChange={(e) => setFile(e.target.files[0])}
                 />
               </div>
 
-              <div className="input-container">
-                <label>Country</label>
-                <input type="password" placeholder="Georgia" />
-              </div>
+              {inputs.map((input) => (
+                <div className="input-container" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
 
               <button>Send</button>
             </form>
